fcb94cf7e9f1ec802fd3cd6eb0c47c2acc17ca33
Panos Bozelos <10163832+bozelosp@users.noreply.github.com>
2025-05-17 06:28:20 +0200

    Refactor graph utilities

--- graph.py
+++ graph.py
@@ -1,292 +1,105 @@
+"""Utilities to create 3-D overlays of neuronal morphologies."""
+
+from __future__ import annotations
+
+import os
 import re
-from math import sqrt
-import sys
-import numpy as np
-from random import randint, uniform
-from math import cos, sin, pi, sqrt, radians, degrees
+from typing import Dict, Iterable, List, Sequence
 
-
-import matplotlib as mpl
-mpl.use('TKAgg')
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import animation
-import matplotlib.pyplot as plt
 from utils import distance, round_to
 
-
-def local_plot(my_file):
-
-        fig = plt.figure()
-        ax = Axes3D(fig)
-        ax = fig.gca(projection='3d')
-
-        x_points=[]
-        y_points=[]
-        z_points=[]
-        diameter=[]
-
-        point_map={}
-        segment_list=[]
-
-        for line in my_file:
-        
-                comment=re.search(r'#', line)
-                p=re.search(r'(\d+) (\d+) (.*?) (.*?) (.*?) (.*?) (-?\d+)', line)
-
-                if comment:
-                        continue
-                elif p:
-
-                        i=int(p.group(1))
-                        l=int(p.group(2))
-                        x=round_to(float(p.group(3)),0.01)
-                        y=round_to(float(p.group(4)),0.01)
-                        z=round_to(float(p.group(5)),0.01)
-                        d=float(p.group(6))
-                        c=int(p.group(7))
-
-                        point_map[i]=[i, l, x, y, z, d, c]
-                        segment_list.append(i)
-
-        for i in segment_list:
-
-                con=point_map[i][6]
-
-                if con!=-1:
-
-                        x=[ point_map[i][2], point_map[con][2] ]
-                        y=[ point_map[i][3], point_map[con][3] ]
-                        z=[ point_map[i][4], point_map[con][4] ]
-
-                        d=point_map[i][5]
-                        
-                        parameters=[x, y, z, d]
-
-                        ax.plot(parameters[0], parameters[1], parameters[2], linewidth=parameters[3], c='b', alpha=1)
-
-        ax.tick_params(labelsize=8)
-
-        # Create an init function and the animate functions.
-        # Both are explained in the tutorial. Since we are changing
-        # the elevation and azimuth and no objects are really
-        # changed on the plot we don't have to return anything from
-        # the init and animate function. (return value is explained
-        # in the tutorial.
-
-        def animate(i):
-            ax.view_init(elev=10., azim=i)
-
-         #Animate
-        #anim = animation.FuncAnimation(fig, animate, frames=800, blit=True)
-         #Save
-        #anim.save('ca3.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
-        
-        plt.show()
-
-def plot_(my_file, my_plot, dendrite_list):
-
-        x_points=[]
-        y_points=[]
-        z_points=[]
-        diameter=[]
-
-        point_map={}
-        segment_list=[]
-
-        for line in my_file:
-        
-                comment=re.search(r'#', line)
-                p=re.search(r'(\d+) (\d+) (.*?) (.*?) (.*?) (.*?) (-?\d+)', line)
-
-                if comment:
-                        continue
-                elif p:
-
-                        i=int(p.group(1))
-                        l=int(p.group(2))
-                        x=round_to(float(p.group(3)),0.01)
-                        y=round_to(float(p.group(4)),0.01)
-                        z=round_to(float(p.group(5)),0.01)
-                        d=float(p.group(6))
-                        c=int(p.group(7))
-
-                        point_map[i]=[i, l, x, y, z, d, c]
-                        segment_list.append(i)
-
-        distance_list=[]
-
-        for i in segment_list:
-
-                con=point_map[i][6]
-
-                if con!=-1:
-
-                        if i in dendrite_list:
-                                distance_list=[]
-
-                        x=[ point_map[i][2], point_map[con][2] ]
-                        y=[ point_map[i][3], point_map[con][3] ]
-                        z=[ point_map[i][4], point_map[con][4] ]
-
-                        di=distance(x[1],x[0],y[1],y[0],z[1],z[0])
-                        distance_list.append(di)
-                        distance_total=sum(distance_list)
-
-                        #print ' %d %d %.2f %.2f %.2f %.2f %d - %.2f' % (point_map[i][0], point_map[i][1], point_map[i][2], point_map[i][3], point_map[i][4], point_map[i][5], point_map[i][6], distance_total)
-                        d=point_map[i][5]
-                        parameters=[x, y, z, d]
-
-                        my_plot.append(parameters)
-
-        return my_plot
-
-def plot_1(my_plot, dend_add3d, all_terminal, dendrite_list):
-
-        x_points=[]
-        y_points=[]
-        z_points=[]
-        diameter=[]
-
-        point_map={}
-        segment_list=[]
-
-        for dend in dendrite_list:
-        
-                for p in dend_add3d[dend]:
-
-                        i=int(p[0])
-                        l=int(p[1])
-                        x=round_to(float(p[2]),0.01)
-                        y=round_to(float(p[3]),0.01)
-                        z=round_to(float(p[4]),0.01)
-                        d=float(p[5])
-                        c=int(p[6])
-
-                        point_map[i]=[i, l, x, y, z, d, c]
-                        segment_list.append(i)
-
-
-        for dend in all_terminal:
-
-                for k in dend_add3d[dend]:
-
-                        i=k[0]
-
-                        con=point_map[i][6]
-
-                        if con!=-1 or con!=1:
-
-                                
-
-                                x=[ point_map[i][2], point_map[con][2] ]
-                                y=[ point_map[i][3], point_map[con][3] ]
-                                z=[ point_map[i][4], point_map[con][4] ]
-
-                                d=point_map[i][5]
-                                parameters=[x, y, z, d]
-
-                                #print parameters
-
-                                my_plot.append(parameters)
-
-        return my_plot
-
-
-def graph(initial_file, modified_file, action, dend_add3d, dendrite_list, directory, file_name):
-
-        #fig = plt.figure()
-        #ax = Axes3D(fig)
-        #ax = fig.gca(projection='3d')
-
-        plot_before=[]
-        plot_after=[]
-
-        plot_before=plot_(initial_file, plot_before, dendrite_list)
-        plot_after=plot_(modified_file, plot_after, dendrite_list)
-        
-        #plot_after=plot_1(plot_after, dend_add3d, all_terminal, dendrite_list)
-
-        if action=='shrink' or action=='remove':
-
-                plot_list=[]
-
-                for x in plot_before:
-                        count=0
-                        for i in plot_after:
-
-                                k=0
-
-                                if x[0][0]==i[0][0]:
-                                        k+=1
-                                if x[1][0]==i[1][0]:
-                                        k+=1
-                                if x[2][0]==i[2][0]:
-                                        k+=1
-
-                                if k==3:
-                                        count+=1
-
-                        if count==0:
-                                plot_list.append(x)
-
-
-                plot_after = plot_list
-
-
-#               plot_after = [x for x in plot_before if x not in plot_after]
-
-        if action=='extend' or action=='branch':
-
-                plot_list=[]
-
-                for x in plot_after:
-                        count=0
-                        for i in plot_before:
-
-                                k=0
-
-                                if x[0][0]==i[0][0]:
-                                        k+=1
-                                if x[1][0]==i[1][0]:
-                                        k+=1
-                                if x[2][0]==i[2][0]:
-                                        k+=1
-
-                                if k==3:
-                                        count+=1
-
-                        if count==0:
-                                plot_list.append(x)
-
-
-                plot_after = plot_list
-
-#               plot_after = [x for x in plot_after if x not in plot_before]
-
-        header_lines=[0,1]
-
-        line_idx=0
-
-        
-        fname=directory+file_name.replace('.swc','') + '_neuron.txt'
-
-        with open(fname, 'w') as f:
-
-            for i in plot_before:
-                if line_idx in header_lines:
-                        pass
-                else:
-                        #ax.plot(i[0], i[1], i[2], linewidth=i[3], c='b', alpha=1)
-                        print(i[0][0], i[1][0], i[2][0], i[0][1], i[1][1], i[2][1], i[3], '0x0000FF', file=f)
-                line_idx+=1
-            line_idx=0
-            for i in plot_after:
-                if line_idx in header_lines:
-                        pass
-                else:
-                        #ax.plot(i[0], i[1], i[2], linewidth=i[3], c='r', alpha=1)
-                        print(i[0][0], i[1][0], i[2][0], i[0][1], i[1][1], i[2][1], i[3], '0xFF0000', file=f)
-                line_idx+=1
-
-        #ax.tick_params(labelsize=8)
-        #plt.show()
+# Regular expression used to parse a single SWC line:
+# index type x y z radius parent
+_LINE_RE = re.compile(r"(\d+) (\d+) (\S+) (\S+) (\S+) (\S+) (-?\d+)")
+
+
+PointMap = Dict[int, List[float]]
+PlotEntry = List[Sequence[float]]
+
+
+def _parse_swc_lines(lines: Iterable[str]) -> tuple[PointMap, List[int]]:
+    """Return a mapping of node indices to coordinates and a list of segments."""
+    point_map: PointMap = {}
+    segment_list: List[int] = []
+    for line in lines:
+        if line.lstrip().startswith("#"):
+            continue
+        m = _LINE_RE.search(line)
+        if not m:
+            continue
+        idx, typ, x, y, z, radius, parent = m.groups()
+        point_map[int(idx)] = [
+            int(idx),
+            int(typ),
+            round_to(float(x), 0.01),
+            round_to(float(y), 0.01),
+            round_to(float(z), 0.01),
+            float(radius),
+            int(parent),
+        ]
+        segment_list.append(int(idx))
+    return point_map, segment_list
+
+
+def _build_plot(point_map: PointMap, segments: List[int], dendrite_list: Sequence[int] | None = None) -> List[PlotEntry]:
+    """Return plot data for ``segments`` using ``point_map``."""
+    plot: List[PlotEntry] = []
+    dist_acc: List[float] = []
+    for idx in segments:
+        parent = point_map[idx][6]
+        if parent == -1:
+            continue
+        if dendrite_list and idx in dendrite_list:
+            dist_acc.clear()
+        x = [point_map[idx][2], point_map[parent][2]]
+        y = [point_map[idx][3], point_map[parent][3]]
+        z = [point_map[idx][4], point_map[parent][4]]
+        dist_acc.append(distance(x[1], x[0], y[1], y[0], z[1], z[0]))
+        plot.append([x, y, z, point_map[idx][5]])
+    return plot
+
+
+def _write_plot(path: str, before: List[PlotEntry], after: List[PlotEntry], skip: int = 2) -> None:
+    """Write combined plot data to ``path``."""
+    with open(path, "w") as f:
+        for idx, entry in enumerate(before):
+            if idx < skip:
+                continue
+            x, y, z, d = entry
+            print(x[0], y[0], z[0], x[1], y[1], z[1], d, "0x0000FF", file=f)
+        for idx, entry in enumerate(after):
+            if idx < skip:
+                continue
+            x, y, z, d = entry
+            print(x[0], y[0], z[0], x[1], y[1], z[1], d, "0xFF0000", file=f)
+
+
+def graph(
+    initial_file: Iterable[str],
+    modified_file: Iterable[str],
+    action: str,
+    dend_add3d: object,
+    dendrite_list: Sequence[int],
+    directory: str,
+    file_name: str,
+) -> None:
+    """Create an overlay plot describing the changes between two SWC files."""
+
+    before_map, before_segments = _parse_swc_lines(initial_file)
+    after_map, after_segments = _parse_swc_lines(modified_file)
+
+    before_plot = _build_plot(before_map, before_segments, dendrite_list)
+    after_plot = _build_plot(after_map, after_segments, dendrite_list)
+
+    if action in {"shrink", "remove"}:
+        after_plot = [p for p in before_plot if p not in after_plot]
+    elif action in {"extend", "branch"}:
+        after_plot = [p for p in after_plot if p not in before_plot]
+
+    fname = os.path.join(directory, file_name.replace(".swc", "") + "_neuron.txt")
+    _write_plot(fname, before_plot, after_plot)
+
+
+__all__ = [
+    "graph",
+]
