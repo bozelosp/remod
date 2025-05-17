efdd1cc5008807bfa26c42c8091ab8ab9611701d
Panos Bozelos <10163832+bozelosp@users.noreply.github.com>
2025-05-17 04:59:34 +0200

    Add write_value helper and use it

--- first_run.py
+++ first_run.py
@@ -12,7 +12,7 @@ from statistics_swc import (
     sholl_intersections,
     branch_order,
 )
-from utils import round_to, write_json
+from utils import round_to, write_json, write_value
 import sys
 import numpy as np
 import os
@@ -190,89 +190,82 @@ for file_name in file_names:
         first_graph(directory, file_name, dendrite_list, dend_add3d, points, parental_points,soma_index) #plots the original and modified tree (overlaying one another)
 
         fnumdend=os.path.join(stats_dir, file_name + '_number_of_all_dendrites.txt')
-        with open(fnumdend, 'w+') as f:
-                print(str(len(dendrite_list)), file=f)
+        write_value(fnumdend, len(dendrite_list))
         average_number_of_all_dendrites.append(len(dendrite_list))
 
         fnumdend=os.path.join(stats_dir, file_name + '_number_of_all_terminal_dendrites.txt')
-        with open(fnumdend, 'w+') as f:
-                print(str(len(all_terminal)), file=f)
+        write_value(fnumdend, len(all_terminal))
         average_number_of_all_terminal_dendrites.append(len(all_terminal))
         
         fnumdend=os.path.join(stats_dir, file_name + '_number_of_basal_dendrites.txt')
-        with open(fnumdend, 'w+') as f:
-                print(str(len(basal)), file=f)
+        write_value(fnumdend, len(basal))
         average_number_of_basal_dendrites.append(len(basal))
         
         fnumdend=os.path.join(stats_dir, file_name + '_number_of_basal_terminal_dendrites.txt')
-        with open(fnumdend, 'w+') as f:
-                print(str(len(basal_terminal)), file=f)
+        write_value(fnumdend, len(basal_terminal))
         average_number_of_basal_terminal_dendrites.append(len(basal_terminal))
         
         fnumdend=os.path.join(stats_dir, file_name + '_number_of_apical_dendrites.txt')
-        with open(fnumdend, 'w+') as f:
-                print(str(len(apical)), file=f)
+        write_value(fnumdend, len(apical))
         average_number_of_apical_dendrites.append(len(apical))
         
         fnumdend=os.path.join(stats_dir, file_name + '_number_of_apical_terminal_dendrites.txt')
-        with open(fnumdend, 'w+') as f:
-                print(str(len(apical_terminal)), file=f)
+        write_value(fnumdend, len(apical_terminal))
         average_number_of_apical_terminal_dendrites.append(len(apical_terminal))
 
         t_length=total_length(dendrite_list, dist)
         ftotlength=os.path.join(stats_dir, file_name + '_all_total_length.txt')
-        with open(ftotlength, 'w+') as f:
-                print(t_length, file=f)
+        write_value(ftotlength, t_length)
         average_t_length.append(t_length)
 
         basal_t_length=total_length(basal, dist)
         ftotblength=os.path.join(stats_dir, file_name + '_basal_total_length.txt')
-        with open(ftotblength, 'w+') as f:
-                print(basal_t_length, file=f)
+        write_value(ftotblength, basal_t_length)
         average_basal_t_length.append(basal_t_length)
 
         apical_t_length=total_length(apical, dist)
         ftotalength=os.path.join(stats_dir, file_name + '_apical_total_length.txt')
-        with open(ftotalength, 'w+') as f:
-                print(apical_t_length, file=f)
+        write_value(ftotalength, apical_t_length)
         average_apical_t_length.append(apical_t_length)
 
         t_area=total_area(dendrite_list, area)
         stats_file_path=os.path.join(stats_dir, file_name + '_all_total_area.txt')
-        with open(stats_file_path, 'w+') as f:
-                print(t_area, file=f)
+        write_value(stats_file_path, t_area)
         average_t_area.append(t_area)
         
         basal_t_area=total_area(basal, area)
         stats_file_path=os.path.join(stats_dir, file_name + '_basal_total_area.txt')
-        with open(stats_file_path, 'w+') as f:
-                print(basal_t_area, file=f)
+        write_value(stats_file_path, basal_t_area)
         average_basal_t_area.append(basal_t_area)
         
         apical_t_area=total_area(apical, area)
         stats_file_path=os.path.join(stats_dir, file_name + '_apical_total_area.txt')
-        with open(stats_file_path, 'w+') as f:
-                print(apical_t_area, file=f)
+        write_value(stats_file_path, apical_t_area)
         average_apical_t_area.append(apical_t_area)
 
         #print list(set([parental_points[x] for x in branch_points]))
         #print len(list(set([parental_points[x] for x in branch_points])))
 
         fnum_all_bpoints=os.path.join(stats_dir, file_name + '_number_of_all_branchpoints.txt')
-        with open(fnum_all_bpoints, 'w+') as f:
-
-                soma=[x[0] for x in soma_index]
-                print(len(list(set([parental_points[x] for x in branch_points if parental_points[x] not in soma]))), file=f)
+        soma=[x[0] for x in soma_index]
+        write_value(
+            fnum_all_bpoints,
+            len(list(set([parental_points[x] for x in branch_points if parental_points[x] not in soma]))),
+        )
         average_num_all_bpoints.append(len(list(set([parental_points[x] for x in branch_points if parental_points[x] not in soma]))))
 
         fnum_basal_bpoints=os.path.join(stats_dir, file_name + '_number_of_basal_branchpoints.txt')
-        with open(fnum_basal_bpoints, 'w+') as f:
-                print(len(list(set([parental_points[x] for x in basal_bpoints if parental_points[x] not in soma]))), file=f)
+        write_value(
+            fnum_basal_bpoints,
+            len(list(set([parental_points[x] for x in basal_bpoints if parental_points[x] not in soma]))),
+        )
         average_num_basal_bpoints.append(len(list(set([parental_points[x] for x in basal_bpoints if parental_points[x] not in soma]))))
 
         fnum_apical_bpoints=os.path.join(stats_dir, file_name + '_number_of_apical_branchpoints.txt')
-        with open(fnum_apical_bpoints, 'w+') as f:
-                print(len(list(set([parental_points[x] for x in apical_bpoints if parental_points[x] not in soma]))), file=f)
+        write_value(
+            fnum_apical_bpoints,
+            len(list(set([parental_points[x] for x in apical_bpoints if parental_points[x] not in soma]))),
+        )
         average_num_apical_bpoints.append(len(list(set([parental_points[x] for x in apical_bpoints if parental_points[x] not in soma]))))
 
         stats_file_path=os.path.join(stats_dir, file_name + '_list_of_all_dendrites.txt')
@@ -572,104 +565,98 @@ print()
 print()
 print("Number of All Dendrites: " + str(average_list(average_number_of_all_dendrites)))
 f_average_number_of_all_dendrites=os.path.join(stats_dir, 'average_number_of_all_dendrites.txt')
-with open(f_average_number_of_all_dendrites, 'w+') as f:
-        (avg1, avg2)=average_list(average_number_of_all_dendrites)
-        print(avg1, avg2, file=f)
+(avg1, avg2)=average_list(average_number_of_all_dendrites)
+write_value(f_average_number_of_all_dendrites, f"{avg1} {avg2}")
 
 print()
 print("Number of All Terminal Dendrites: " + str(average_list(average_number_of_all_terminal_dendrites)))
 f_average_number_of_all_terminal_dendrites=os.path.join(stats_dir, 'average_number_of_all_terminal_dendrites.txt')
-with open(f_average_number_of_all_terminal_dendrites, 'w+') as f:
-        (avg1, avg2)=average_list(average_number_of_all_terminal_dendrites)
-        print(avg1, avg2, file=f)
+(avg1, avg2)=average_list(average_number_of_all_terminal_dendrites)
+write_value(f_average_number_of_all_terminal_dendrites, f"{avg1} {avg2}")
 
 print()
 print("Number of Basal Dendrites: " + str(average_list(average_number_of_basal_dendrites)))
 f_average_number_of_basal_dendrites=os.path.join(stats_dir, 'average_number_of_basal_dendrites.txt')
-with open(f_average_number_of_basal_dendrites, 'w+') as f:
-        (avg1, avg2)=average_list(average_number_of_basal_dendrites)
-        print(avg1, avg2, file=f)
+(avg1, avg2)=average_list(average_number_of_basal_dendrites)
+write_value(f_average_number_of_basal_dendrites, f"{avg1} {avg2}")
 
 print()
 print("Number of Basal Terminal Dendrites: " + str(average_list(average_number_of_basal_terminal_dendrites)))
 f_average_number_of_basal_terminal_dendrites=os.path.join(stats_dir, 'average_number_of_basal_terminal_dendrites.txt')
-with open(f_average_number_of_basal_terminal_dendrites, 'w+') as f:
-        (avg1, avg2)=average_list(average_number_of_basal_terminal_dendrites)
-        print(avg1, avg2, file=f)
+(avg1, avg2)=average_list(average_number_of_basal_terminal_dendrites)
+write_value(f_average_number_of_basal_terminal_dendrites, f"{avg1} {avg2}")
 
 print()
 print("Number of Apical Dendrites: " + str(average_list(average_number_of_apical_dendrites)))
 f_average_number_of_apical_dendrites=os.path.join(stats_dir, 'average_number_of_apical_dendrites.txt')
-with open(f_average_number_of_apical_dendrites, 'w+') as f:
-        (avg1, avg2)=average_list(average_number_of_apical_dendrites)
-        print(avg1, avg2, file=f)
+(avg1, avg2)=average_list(average_number_of_apical_dendrites)
+write_value(f_average_number_of_apical_dendrites, f"{avg1} {avg2}")
 
 print()
 print("Number of Apical Terminal Dendrites: " + str(average_list(average_number_of_apical_terminal_dendrites)))
 f_average_number_of_apical_terminal_dendrites=os.path.join(stats_dir, 'average_number_of_apical_terminal_dendrites.txt')
-with open(f_average_number_of_apical_terminal_dendrites, 'w+') as f:
-        (avg1, avg2)=average_list(average_number_of_apical_terminal_dendrites)
-        print(avg1, avg2, file=f)
+(avg1, avg2)=average_list(average_number_of_apical_terminal_dendrites)
+write_value(f_average_number_of_apical_terminal_dendrites, f"{avg1} {avg2}")
 
 print()
 print("Total Length (all dendrites): " + str(average_list(average_t_length)))
 f_average_total_length=os.path.join(stats_dir, 'average_all_total_length.txt')
-with open(f_average_total_length, 'w+') as f:
-        (avg1, avg2)=average_list(average_t_length)
-        print(avg1, avg2, file=f)
+(avg1, avg2)=average_list(average_t_length)
+write_value(f_average_total_length, f"{avg1} {avg2}")
 
 print()
 print("Total Length (basal dendrites): " + str(average_list(average_basal_t_length)))
 f_average_total_basal_length=os.path.join(stats_dir, 'average_basal_total_length.txt')
-with open(f_average_total_basal_length, 'w+') as f:
-        (avg1, avg2)=average_list(average_basal_t_length)
-        print(avg1, avg2, file=f)
+(avg1, avg2)=average_list(average_basal_t_length)
+write_value(f_average_total_basal_length, f"{avg1} {avg2}")
 
 print()
 print("Total Length (apical dendrites): " + str(average_list(average_apical_t_length)))
 f_average_total_apical_length=os.path.join(stats_dir, 'average_apical_total_length.txt')
-with open(f_average_total_apical_length, 'w+') as f:
-        (avg1, avg2)=average_list(average_apical_t_length)
-        print(avg1, avg2, file=f)
+(avg1, avg2)=average_list(average_apical_t_length)
+write_value(f_average_total_apical_length, f"{avg1} {avg2}")
 
 print()
 print("Total Area (all dendrites): " + str(average_list(average_t_area)))
 f_average_total_area=os.path.join(stats_dir, 'average_all_total_area.txt')
-with open(f_average_total_area, 'w+') as f:
-        (avg1, avg2)=average_list(average_t_area)
-        print(avg1, avg2, file=f)
+(avg1, avg2)=average_list(average_t_area)
+write_value(f_average_total_area, f"{avg1} {avg2}")
 
 print()
 print("Total Area (basal dendrites): " + str(average_list(average_basal_t_area)))
 f_average_total_basal_area=os.path.join(stats_dir, 'average_basal_total_area.txt')
-with open(f_average_total_basal_area, 'w+') as f:
-        (avg1, avg2)=average_list(average_basal_t_area)
-        print(avg1, avg2, file=f)
+(avg1, avg2)=average_list(average_basal_t_area)
+write_value(f_average_total_basal_area, f"{avg1} {avg2}")
 
 print()
 print("Total Area (apical dendrites): " + str(average_list(average_apical_t_area)))
 f_average_total_apical_area=os.path.join(stats_dir, 'average_apical_total_area.txt')
-with open(f_average_total_apical_area, 'w+') as f:
-        (avg1, avg2)=average_list(average_apical_t_area)
-        print(avg1, avg2, file=f)
+(avg1, avg2)=average_list(average_apical_t_area)
+write_value(f_average_total_apical_area, f"{avg1} {avg2}")
 
 print()
 print("Number of all Branch Points: " + str(average_list(average_num_all_bpoints)[0]), str(average_list(average_num_all_bpoints)[1]))
 f_average_num_all_bpoints=os.path.join(stats_dir, 'average_number_of_all_branchpoints.txt')
-with open(f_average_num_all_bpoints, 'w+') as f:
-        print(str(average_list(average_num_all_bpoints)[0]), str(average_list(average_num_all_bpoints)[1]), file=f)
+write_value(
+    f_average_num_all_bpoints,
+    f"{average_list(average_num_all_bpoints)[0]} {average_list(average_num_all_bpoints)[1]}",
+)
 
 print()
 print("Number of all Basal Branch Points: " + str(average_list(average_num_basal_bpoints)[0]), str(average_list(average_num_basal_bpoints)[1]))
 f_average_num_basal_bpoints=os.path.join(stats_dir, 'average_number_of_basal_branchpoints.txt')
-with open(f_average_num_basal_bpoints, 'w+') as f:
-        print(str(average_list(average_num_basal_bpoints)[0]), str(average_list(average_num_basal_bpoints)[1]), file=f)
+write_value(
+    f_average_num_basal_bpoints,
+    f"{average_list(average_num_basal_bpoints)[0]} {average_list(average_num_basal_bpoints)[1]}",
+)
 
 print()
 print("Number of all Apical Branch Points: " + str(average_list(average_num_apical_bpoints)[0]), str(average_list(average_num_apical_bpoints)[1]))
 f_average_num_apical_bpoints=os.path.join(stats_dir, 'average_number_of_apical_branchpoints.txt')
-with open(f_average_num_apical_bpoints, 'w+') as f:
-        print(str(average_list(average_num_apical_bpoints)[0]), str(average_list(average_num_apical_bpoints)[1]), file=f)
+write_value(
+    f_average_num_apical_bpoints,
+    f"{average_list(average_num_apical_bpoints)[0]} {average_list(average_num_apical_bpoints)[1]}",
+)
 
 print()
 print("Average Number of All Dendrites per Branch Order: ") 
